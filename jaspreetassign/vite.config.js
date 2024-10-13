a325117a74d6d9f3418335103c6847a895cf4851
jassreet <95341734+jassreet@users.noreply.github.com>
2024-10-13 11:23:52 -0400

    vite.config.js

--- jaspreetassign/vite.config.js
+++ jaspreetassign/vite.config.js
@@ -1,8 +1,7 @@
-import { defineConfig } from 'vitest/config';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  test: {
-    environment: 'jsdom', // Make sure this is present for React testing
-  },
-});
-
+  plugins: [react()],
+})
